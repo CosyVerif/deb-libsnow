1287997ab8e6c176ce05b376d3d2591e86038eab
rgouicem <redha.gouicem@gmail.com>
2014-05-22 17:34:13 +0200

    Updated package building scripts.

--- build_deb.sh
+++ build_deb.sh
@@ -7,16 +7,15 @@ SRCDIR="$PACKAGENAME-$VERSION"
 TARBALL="$PACKAGENAME""_$VERSION"
 DEBTYPE=l
 MAIL=redha.gouicem@gmail.com
-URL='http://www.ljdfd.fr/testpag.html'
-#LICENCE=-c gpl;
-INCLUDEFILES="lib/*.h"
-LIBFILES="lib/libPNet.a"
+HOMEPAGE='https://github.com/CosyVerif/deb-libsnow'
+export HOMEPAGE;
+LOGFILE="correct_lintian.log"
+export LOGFILE;
+#LICENCE="-c gpl";
+INCLUDEFILES="lib/*.h interfaces/*.h"
+LIBFILES="lib/*.a"
 SHORTDESC="short description blalala"
 LONGDESC=" long descritpion blablanazjnqsoncqslcqpsfjdbfdcdjvns lq qs q qs qiqs,klcl nfiqd qc,qsin qi ncqcdn snv iqj q cqn"
-
-echo "!!!!!!!!!!!!!!!!!!!!!!       VARS !     !!!!!!!!!!!!!!!!!!!!!!!!!";
-echo "srcdir = $SRCDIR";
-echo "tarball = $PACKAGENAME""_$VERSION"
 
 # cleaning
 rm -rf $SRCDIR/debian;
@@ -34,7 +33,6 @@ mv "$PACKAGENAME"1.install $PACKAGENAME.install;
 
 sed -i -r -e s/"$PACKAGENAME"BROKEN/"$PACKAGENAME"/g control
 
-
 > $PACKAGENAME.install
 for i in $LIBFILES; do
     echo "$i usr/lib" >> $PACKAGENAME.install;
@@ -44,4 +42,9 @@ for i in $INCLUDEFILES; do
     echo "$i usr/include/" >> $PACKAGENAME-dev.install;
 done;
 
-cd .. && debuild -us -uc;
+# building source package
+cd .. && debuild -S -us -uc;
+# correcting common errors
+../correct_lintian.pl ../$TARBALL-1_source.build;
+# building deb packages
+debuild -us -uc;
